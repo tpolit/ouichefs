dc1ad4e03699e2dd70734e52e8ec6d35adcc2bff
titouan.polit <titouan.polit@orange.fr>
2022-05-30 09:08:59 +0200

    correctifs ioctl

--- ioctl/ex34.c
+++ ioctl/ex34.c
@@ -26,8 +26,7 @@ static long do_version(int ver, char *cible)
 	int nb_prev, current_index, i;
 	/* uint32_t inode_block, inode_shift; */
 
-	/*cible_f = filp_open("/share/ouichefs-master/mkfs/ouiche_dir/file",
-	O_RDONLY, 0400); */
+	pr_info("******************ioctl:version*****************\n");
 	cible_f = filp_open(cible, O_RDONLY, 0400);
 	if (IS_ERR(cible_f)) {
 		pr_info("Open error\n");
@@ -35,33 +34,19 @@ static long do_version(int ver, char *cible)
 	}
 	pr_info("En cherche la version -%d\n", ver);
 	c_inode = cible_f->f_inode;
-
-	/* accés à l'inode sur disque */
-	/* inode_block = (c_inode->i_ino / OUICHEFS_INODES_PER_BLOCK) + 1;
-	inode_shift = c_inode->i_ino % OUICHEFS_INODES_PER_BLOCK;
-
-	temp_bh = sb_bread(c_inode->i_sb, inode_block);
-	if (!temp_bh) {
-		return -EIO;
-	}
-	d_inode = (struct ouichefs_inode *)temp_bh->b_data;
-	d_inode += inode_shift;
-	current_index = le32_to_cpu(d_inode->last_index_block);
-	brelse(temp_bh);*/
-
 	co_inode = OUICHEFS_INODE(c_inode);
 	pr_info("Before: current index %d, last %d\n",
 		co_inode->index_block, co_inode->last_index_block);
 	current_index = co_inode->last_index_block;
 	for (nb_prev = ver; nb_prev > 0; nb_prev--) {
-		/* pr_info("Current index of inode %ld is: %d\n",
-			c_inode->i_ino, current_index); */
+		/* pr_info("Current index of inode %ld is: %d\n",\
+		 c_inode->i_ino, current_index); */
 		temp_bh = sb_bread(c_inode->i_sb, current_index);
 		if (!temp_bh)
 			return -EIO;
 		block = (struct ouichefs_file_index_block *)temp_bh->b_data;
-		/* arrivé à la 1ére version et on veut
-		quand même reculer plus */
+		/* arrivé à la 1ére version et on veut quand même reculer
+		 plus */
 		if (block->prev == -1)
 			return -1;
 		current_index = block->prev;
@@ -73,17 +58,6 @@ static long do_version(int ver, char *cible)
 	mark_inode_dirty(c_inode);
 	pr_info("After: Current index of inode %ld is: %d\n",
 		c_inode->i_ino, co_inode->index_block);
-
-	/* mettre à jour l'inode sur disque */
-	/*temp_bh = sb_bread(c_inode->i_sb, inode_block);
-	if (!temp_bh) {
-		return -EIO;
-	}
-	d_inode = (struct ouichefs_inode *)temp_bh->b_data;
-	d_inode += inode_shift;
-	d_inode->index_block = cpu_to_le32(current_index);
-	brelse(temp_bh);*/
-
 	/* verifier les blocs de donnée */
 	temp_bh = sb_bread(c_inode->i_sb, co_inode->index_block);
 	if (!temp_bh)
@@ -94,6 +68,7 @@ static long do_version(int ver, char *cible)
 		pr_info("Bloc %d\n", block->blocks[i]);
 	brelse(temp_bh);
 	fput(cible_f);
+	pr_info("******************ioctl:version*****************\n");
 	return 0;
 }
 
@@ -101,8 +76,8 @@ static long do_rewind(char *cible)
 {
 	/*
 	rendre l'index courant le last index
-	tout en liberant tout les blocs utilisé par les
-	versions entre now et last(inclus)
+	tout en liberant tout les blocs utilisé
+	par les versions entre now et last(inclus)
 	*/
 	struct inode *c_inode;
 	struct ouichefs_inode_info *co_inode;
@@ -111,6 +86,7 @@ static long do_rewind(char *cible)
 	struct file *cible_f;
 	int current_index, tofree_index, i;
 
+	pr_info("******************ioctl:rewind*****************\n");
 	cible_f = filp_open(cible, O_RDONLY, 0400);
 	if (IS_ERR(cible_f)) {
 		pr_info("Open error\n");
@@ -143,11 +119,11 @@ static long do_rewind(char *cible)
 	pr_info("Current index = %d, last index = %d\n",
 		co_inode->index_block, co_inode->last_index_block);
 	mark_inode_dirty(c_inode);
+	pr_info("******************ioctl:rewind*****************\n");
 	return 0;
 }
 
-static long ul_ioctl(struct file *file, unsigned int cmd,
-	unsigned long arg)
+static long ul_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	struct parameters *p;
 	struct parameters_rewind *pr;
@@ -164,15 +140,14 @@ static long ul_ioctl(struct file *file, unsigned int cmd,
 			pr_err("Size undifined\n");
 			return -1;
 		}
-		p = (struct parameters *)
-			kmalloc(sizeof(struct parameters) +
-			size_filename, GFP_KERNEL);
-		ret = copy_from_user(p, (struct parameters *)arg,
-			sizeof(struct parameters) + size_filename);
+		p = (struct parameters *)kmalloc(sizeof(struct\
+		 parameters) + size_filename, GFP_KERNEL);
+		ret = copy_from_user(p, (struct parameters *)arg,\
+		 sizeof(struct parameters) + size_filename);
 		if (ret)
 			pr_err("Read problem, ret = %d\n", ret);
-		pr_info("Parameters: version = %d, filename = %s\n",
-			p->version, p->cible);
+		pr_info("Parameters: version = %d, filename = %s\n",\
+		 p->version, p->cible);
 		do_version(p->version, p->cible);
 		size_filename = -1;
 		break;
@@ -181,11 +156,10 @@ static long ul_ioctl(struct file *file, unsigned int cmd,
 			pr_err("Size undifined\n");
 			return -1;
 		}
-		pr = (struct parameters_rewind *)
-			kmalloc(sizeof(struct parameters_rewind) +
-				size_filename, GFP_KERNEL);
-		ret = copy_from_user(pr, (struct parameters_rewind *)arg,
-			sizeof(struct parameters_rewind) + size_filename)
+		pr = (struct parameters_rewind *)kmalloc(sizeof(struct\
+		 parameters_rewind) + size_filename, GFP_KERNEL);
+		ret = copy_from_user(pr, (struct parameters_rewind *)arg,\
+		 sizeof(struct parameters_rewind) + size_filename);
 		if (ret)
 			pr_err("Read problem, ret = %d\n", ret);
 		pr_info("Parameters: filename = %s\n", pr->cible);
@@ -201,9 +175,9 @@ static long ul_ioctl(struct file *file, unsigned int cmd,
 
 static int __init current_view_init(void)
 {
-	const struct file_operations file_op;
-
-	file_op.unlocked_ioctl = ul_ioctl;
+	static  const struct file_operations file_op = {
+		.unlocked_ioctl = ul_ioctl
+		};
 	nb = register_chrdev(0, "current_view", &file_op);
 	pr_info("numero de l'ioctl : %d\n", nb);
 	return 0;
--- ioctl/use_rewind.c
+++ ioctl/use_rewind.c
@@ -7,39 +7,40 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*
-    argv doit contenir le path vers le fichier visé,
-    puis le nombre de version en arriere que l'on souhaite
-*/
-int main(int argc, char **argv)
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv doit contenir le path vers le fichier visé, puis le nombre de version
+ * @return int 
+ */
+int main(int argc, char** argv)
 {
-	int fd;
-	struct parameters_rewind *p;
-	int ret = 0;
-	int size_filename;
+    int fd;
+    struct parameters_rewind *p;
+    int ret = 0;
+    int size_filename;
 
-	if (argc < 2) {
-		printf("Paramètres manquants\n");
-		goto end;
-	}
-	size_filename = strlen(argv[1]) + 1; /* inclure le \0 */
-	p->size = size_filename;
-	p = malloc(sizeof(struct parameters) + size_filename *
-		(sizeof(char)));
-	strcpy(p->cible, argv[1]);
-	fd = open("/dev/current_view", O_RDWR);
-	ret = ioctl(fd, SIZE, &size_filename);
-	if (ret == -1) {
-		printf("Error ioctl size path\n");
-		goto desalloc;
-	}
-	ret = ioctl(fd, REWIND, p);
-	if (ret == -1)
-		printf("Error ioctl version\n");
-
-desalloc:
+    if (argc < 2) {
+	printf("Paramètres manquants\n");
+	goto end;
+    }
+    size_filename = strlen(argv[1]) + 1; /* inclure le \0 */
+    p->size = size_filename;
+    p = malloc(sizeof(struct parameters)+size_filename*(sizeof(char)));
+    strcpy(p->cible, argv[1]);
+    fd = open("/dev/current_view",O_RDWR);
+    ret = ioctl(fd, SIZE, &size_filename);
+    if (ret == -1) {
+	printf("Error ioctl size path\n");
+	goto desalloc;
+    }
+    ret = ioctl(fd, REWIND, p);
+    if (ret == -1)
+	printf("Error ioctl version\n");
+    desalloc:
 	free(p);
-end:
+    end:
 	printf("End of user program\n");
 	return 0;
 }
